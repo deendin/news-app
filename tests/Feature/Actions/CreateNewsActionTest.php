0a76f7e1943a6bbc7db9eada4ea72c915e147cbf
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2023-01-16 07:31:15 +0000

    Added more test cases to CreateNewsActionTest that tests if an email is dispatched when a news is created

--- tests/Feature/Actions/CreateNewsActionTest.php
+++ tests/Feature/Actions/CreateNewsActionTest.php
@@ -3,13 +3,40 @@
 namespace Tests\Feature\Actions;
 
 use App\Actions\CreateNewsAction;
+use App\Mail\SendNewsCreatedMail;
 use App\Models\News;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class CreateNewsActionTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Mail::fake();
+    }
+
+    /** @test */
+    public function an_email_is_dispatched_when_a_news_is_created()
+    {
+        $data = News::factory()->make();
+
+        $news = resolve(CreateNewsAction::class)->create(
+            $this->user(),
+            $data->toArray()
+        );
+
+        Mail::assertQueued(SendNewsCreatedMail::class, function (SendNewsCreatedMail $mail) use ($news) {
+            expect($mail->news->is($news))->toBeTrue;
+
+            return true;
+        });
+    }
+
 
     /** @test */
     public function given_a_news_it_will_generate_a_new_news_in_the_system()
