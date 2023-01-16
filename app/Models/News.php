a7432d56b865b5b0e8df92721018db141ce40c9c
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2023-01-16 07:14:26 +0000

    Added first new relationship into News model

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
+use App\Traits\HasOlderRecords;
 use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
     use HasFactory;
     use HasUuid;
+    use SoftDeletes;
+    use HasOlderRecords;
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
